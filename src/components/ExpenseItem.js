b5bddc9b60ce0d67bb39ba7354be81ccf297cfb2
Amit <amit78347@gmail.com>
2023-08-18 16:08:55 +0530

    Css code added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
+import './ExpenseItem.css';
+
+
+
 function ExpenseItem() {
-    return <div>
-        <h2>Expense Items</h2>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
+    return <div  className="expense-item">
+        <div>March 28th 2023</div>
+        <div className="expense-item__description">
+        <h2 className="expense-item h2">Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+        </div>
+        
     </div>}
 
     export default ExpenseItem;
