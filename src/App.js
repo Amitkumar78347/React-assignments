cb1721d837cb96b5d2423dc66a39fce230c969b6
Amit <amit78347@gmail.com>
2023-08-20 01:01:14 +0530

    Array mapping

--- src/App.js
+++ src/App.js
@@ -28,14 +28,25 @@ function App() {
   ];
   return (
     <div>
-      <h1>Let's get started</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}  LocationOfExpenditure={expenses[0].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}  LocationOfExpenditure={expenses[1].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} LocationOfExpenditure={expenses[2].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} LocationOfExpenditure={expenses[3].LocationOfExpenditure}></ExpenseItem>
-    
-    </div>
-  );
+    <h1>Let's get started</h1>
+    {expenses.map((expense) => (
+      <ExpenseItem
+        
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        LocationOfExpenditure={expense.LocationOfExpenditure}
+      />
+    ))}
+  </div>
+);
 }
+
+
+
+
+
+
+
 
 export default App;
