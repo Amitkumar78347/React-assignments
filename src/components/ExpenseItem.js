caec078ca4aeecc46d228f4ce6f54ac4f4171e20
Amit <amit78347@gmail.com>
2023-08-20 13:43:13 +0530

    creating separate component for date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,18 @@
+import ExpenseDate from './ExpenseDate';
+
 import './ExpenseItem.css';
 
 
 
 function ExpenseItem(props) {
 
-    const month=props.date.toLocaleString('en-US',{month:'long'});
-    const day=props.date.toLocaleString('en-US',{day:'2-digit'});
-    const year=props.date.getFullYear();
+
   
 
 
     return <div  className="expense-item">
-        <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-
-
-        </div>
+        <ExpenseDate date={props.date}></ExpenseDate>
+       
         <div className="expense-item__description">
         <h2 className="expense-item h2">{props.title}</h2>
         <h2 className="expense-item h2">{props.LocationOfExpenditure}</h2>
