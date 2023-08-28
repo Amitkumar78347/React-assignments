53f5a0bf08397fe73e7416995c3c82ef9ffb5b0b
Amit <amit78347@gmail.com>
2023-08-28 15:18:22 +0530

    useState() hook used

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 
-
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css';
@@ -7,26 +7,29 @@ import './ExpenseItem.css';
 
 
 function ExpenseItem(props) {
-    const handleDeleteExpense = () => {
-        const expenseItemElement = document.querySelector('.expense-item');
-        if (expenseItemElement) {
-          expenseItemElement.remove();
-        }
-      };
+
+  const [title,setTitle]= useState(props.title);
 
   
+  const clickHandler = () => {
+    setTitle('updated');
+    console.log(title);
+  };
 
 
-    return <div  className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
-       
-        <div className="expense-item__description">
-        <h2 className="expense-item h2">{props.title}</h2>
-        <h2 className="expense-item h2">{props.LocationOfExpenditure}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        </div>
-        <button onClick={handleDeleteExpense}>Delete Expense</button>
-        
-    </div>}
 
-    export default ExpenseItem;
+
+  return <div className="expense-item">
+    <ExpenseDate date={props.date}></ExpenseDate>
+
+    <div className="expense-item__description">
+      <h2 className="expense-item h2">{title}</h2>
+      <h2 className="expense-item h2">{props.LocationOfExpenditure}</h2>
+      <div className="expense-item__price">${props.amount}</div>
+    </div>
+    <button onClick={clickHandler}>Change Title</button>
+
+  </div>
+}
+
+export default ExpenseItem;
